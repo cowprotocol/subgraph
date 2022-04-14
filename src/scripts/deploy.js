ec89f2b46c919aa4fb27a079df800fc6ad864baa
Gabriel Nicolás González <gabrielnicolasgonzalez@gmail.com>
2022-04-14 14:37:18 -0300

    removing prepare.js and preventing deploy to fail silent for displaying errors when happens

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -1,8 +1,8 @@
 const assert = require('assert')
-const {series} = require('async')
+const { series } = require('async')
 const { default: chalk } = require('chalk')
-const {exec} = require('child_process')
-const {confirm} = require('./utils')
+const { exec } = require('child_process')
+const { confirm } = require('./utils')
 
 require('dotenv').config()
 
@@ -16,11 +16,13 @@ console.log(`Prepare subgraph config for network ${NETWORK}`)
 console.log(`  Generate subgraph.yaml`)
 
 series([
-  (callback) => exec(`yarn prepare:mainnet`, null, callback),
+  (callback) => exec(`mustache config/${NETWORK}.json subgraph.yaml.mustache > subgraph.yaml`, null, callback),
+
+  (callback) => exec(`mustache config/${NETWORK}.json schema.graphql.mustache > schema.graphql`, null, callback),
 
   (callback) => {
     console.log('\n📝 Generate code from your GraphQL schema')
-    return exec('yarn codegen', undefined, callback)    
+    return exec('yarn codegen', undefined, callback)
   },
 
   (callback) => {
@@ -30,19 +32,27 @@ series([
 `)
     confirm({
       message: chalk.red('Are you sure you want to deploy?'),
-      callbackYes() {      
-        exec(`graph deploy ${SUBGRAPH} --node https://api.thegraph.com/deploy/`)
-        console.log(`\n🚀 Deploy graph to https://thegraph.com/hosted-service/subgraph/${SUBGRAPH}`)  
+      callbackYes() {
+        exec(`graph deploy ${SUBGRAPH} --node https://api.thegraph.com/deploy/ --ipfs https://api.thegraph.com/ipfs/`, (error) => {
+          if (error) {
+            console.log(`${chalk.red('\n\n🚩 There was an error deploying the subgraph 🚩')} \n\n`)
+            console.error(`${error}`)
+            return
+          }
+        })
+        console.log(`\n🚀 Deploy graph to https://thegraph.com/hosted-service/subgraph/${SUBGRAPH}`)
         callback()
       },
       callbackNo() {
         console.log('\n🤗 Ok, not deploying then'); callback()
       }
-    })    
+    })
   }
-], function(error) {
-  if(error) {
-    console.log(`${chalk.red('\n\n🚩 There was an error preparing the subgraph 🚩')}: \n\n ${error}`)
+], (error) => {
+  if (error) {
+    console.log(`${chalk.red('\n\n🚩 There was an error preparing the subgraph 🚩')} \n\n`)
+    console.error(`${error}`)
     return
   }
+  return
 });

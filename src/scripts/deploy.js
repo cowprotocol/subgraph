7e38d957a63c5142cf5a654f933c6ec47354af17
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-05-02 10:39:04 +0200

    Update deploy script to use new approach

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -1,56 +1,90 @@
-const assert = require('assert')
-const { series } = require('async')
-const { default: chalk } = require('chalk')
-const { exec } = require('child_process')
-const { confirm } = require('./utils')
+const assert = require("assert");
+const { series } = require("async");
+const { default: chalk } = require("chalk");
+const { exec } = require("child_process");
+const { confirm } = require("./utils");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const NETWORK = process.env.NETWORK
-assert(NETWORK, 'NETWORK env var is required')
+const NETWORK = process.env.NETWORK;
+assert(NETWORK, "NETWORK env var is required");
 
-const SUBGRAPH = process.env.SUBGRAPH
-assert(SUBGRAPH, 'SUBGRAPH env var is required')
+const SUBGRAPH = process.env.SUBGRAPH;
+assert(SUBGRAPH, "SUBGRAPH env var is required");
 
-console.log(`Prepare subgraph config for network ${NETWORK}`)
-console.log(`  Generate subgraph.yaml`)
+const AUTH_TOKEN = process.env.AUTH_TOKEN;
+assert(AUTH_TOKEN, "AUTH_TOKEN env var is required");
 
-series([
-  (callback) => exec(`mustache config/${NETWORK}.json subgraph.yaml.mustache > subgraph.yaml`, null, callback),
+const VERSION = process.env.VERSION;
+assert(VERSION, "VERSION env var is required");
 
-  (callback) => {
-    console.log('\n📝 Generate code from your GraphQL schema')
-    return exec('yarn codegen', undefined, callback)
-  },
+console.log(`Prepare subgraph config for network ${NETWORK}`);
+console.log(`  Generate subgraph.yaml`);
 
-  (callback) => {
-    console.log(`\n⬆️  Deploying graph:
-      ${chalk.blue('Subgraph')}: ${SUBGRAPH}
-      ${chalk.blue('Network')}: ${NETWORK}
-`)
-    confirm({
-      message: chalk.red('Are you sure you want to deploy?'),
-      callbackYes() {
-        exec(`graph deploy ${SUBGRAPH} --product hosted-service`, (error) => {
-          if (error) {
-            console.log(`${chalk.red('\n\n🚩 There was an error deploying the subgraph 🚩')} \n\n`)
-            console.error(`${error}`)
-            return
-          }
-        })
-        console.log(`\n🚀 Deploy graph to https://thegraph.com/hosted-service/subgraph/${SUBGRAPH}`)
-        callback()
-      },
-      callbackNo() {
-        console.log('\n🤗 Ok, not deploying then'); callback()
-      }
-    })
+series(
+  [
+    (callback) => {
+      return exec(
+        `mustache config/${NETWORK}.json subgraph.yaml.mustache > subgraph.yaml`,
+        callback
+      );
+    },
+    (callback) => {
+      console.log("\n📝 Generate code from your GraphQL schema");
+      return exec("yarn codegen", callback);
+    },
+    (callback) => {
+      console.log("\n📝 Build subgraph");
+      return exec("yarn build", callback);
+    },
+    (callback) => {
+      console.log("\n📝 Authenticate with the Graph Studio");
+      return exec(`graph auth --studio ${AUTH_TOKEN}`, callback);
+    },
+    (callback) => {
+      console.log(`\n⬆️  Deploying graph:
+        ${chalk.blue("Subgraph")}: ${SUBGRAPH}
+        ${chalk.blue("Network")}: ${NETWORK}
+      `);
+      confirm({
+        message: chalk.red("Are you sure you want to deploy?"),
+        callbackYes() {
+          exec(
+            `graph deploy --studio ${SUBGRAPH} --version-label ${VERSION}`,
+            (error) => {
+              if (error) {
+                console.log(
+                  `${chalk.red(
+                    "\n\n🚩 There was an error deploying the subgraph 🚩"
+                  )} \n\n`
+                );
+                console.error(`${error}`);
+                return;
+              }
+            }
+          );
+          console.log(
+            `\n🚀 Deploy graph to https://thegraph.com/studio/subgraph/${SUBGRAPH}`
+          );
+          callback();
+        },
+        callbackNo() {
+          console.log("\n🤗 Ok, not deploying then");
+          callback();
+        },
+      });
+    },
+  ],
+  (error) => {
+    if (error) {
+      console.log(
+        `${chalk.red(
+          "\n\n🚩 There was an error preparing the subgraph 🚩"
+        )} \n\n`
+      );
+      console.error(`${error}`);
+      return;
+    }
+    return;
   }
-], (error) => {
-  if (error) {
-    console.log(`${chalk.red('\n\n🚩 There was an error preparing the subgraph 🚩')} \n\n`)
-    console.error(`${error}`)
-    return
-  }
-  return
-});
+);
